3f91780aca8de992a50b1ee7fdcd6e08e0b029b1
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2024-01-04 14:56:30 +0100

    Sale order:Replace the report attached to mail by the sealed one.

--- __manifest__.py
+++ __manifest__.py
@@ -2,8 +2,8 @@
 
 {
     'name': 'Sale order signed and sealed',
-    'version': '1.0.1.1',
-    'author':'Soft-integration',
+    'version': '1.0.1.2',
+    'author': 'Soft-integration',
     'category': 'Sale',
     'summary': 'Sale order signed and sealed',
     'description': "",
@@ -13,7 +13,8 @@
     'data': [
         'views/res_users_views.xml',
         'report/sale_report.xml',
-        'report/sale_report_templates.xml'
+        'report/sale_report_templates.xml',
+        'data/mail_template_data.xml',
     ],
     'installable': True,
     'application': False,
--- models/sale_order.py
+++ models/sale_order.py
@@ -15,3 +15,11 @@ class SaleOrder(models.Model):
     def _compute_document_seal(self):
         for each in self:
             each.document_seal = False
+
+    def _find_mail_template(self, force_confirmation_template=False):
+        template_id = super(SaleOrder,self)._find_mail_template(force_confirmation_template=force_confirmation_template)
+        if template_id == self.env['ir.model.data']._xmlid_to_res_id('sale.mail_template_sale_confirmation', raise_if_not_found=False):
+            template_id = self.env['ir.model.data']._xmlid_to_res_id('sale_order_report_sealed.mail_template_sale_confirmation_with_seal', raise_if_not_found=False)
+        elif template_id == self.env['ir.model.data']._xmlid_to_res_id('sale.email_template_edi_sale', raise_if_not_found=False):
+            template_id = self.env['ir.model.data']._xmlid_to_res_id('sale_order_report_sealed.email_template_edi_sale_with_seal', raise_if_not_found=False)
+        return template_id
