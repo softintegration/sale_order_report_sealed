043db0c957a50ba5472359c31cb892e7327cedd4
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2024-01-05 14:45:53 +0100

    Sale order:Add the seal of the sale order

--- __manifest__.py
+++ __manifest__.py
@@ -2,13 +2,13 @@
 
 {
     'name': 'Sale order signed and sealed',
-    'version': '1.0.1.3',
+    'version': '1.0.1.4',
     'author': 'Soft-integration',
     'category': 'Sale',
     'summary': 'Sale order signed and sealed',
     'description': "",
     'depends': [
-        'sale','res_user_document_signature'
+        'sale','res_user_document_signature','res_company_document_seal'
     ],
     'data': [
         'report/sale_report.xml',
